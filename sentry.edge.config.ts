0bf223540635d49859f2489b9458ccd5421dcc3b
rhorba <mohamedd.rhorba@gmail.com>
2026-03-17 17:47:54 +0000

    fix: hardcode Sentry DSN in config files

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -1,7 +1,7 @@
 import * as Sentry from '@sentry/nextjs'
 
 Sentry.init({
-  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+  dsn: 'https://3a47c0b06fea8fa7b7055eacdbe6ae82@o4511060888256512.ingest.de.sentry.io/4511060895727696',
   enabled: process.env.NODE_ENV === 'production',
   tracesSampleRate: 0.2,
 })
